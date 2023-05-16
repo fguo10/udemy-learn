f1b5bb4023a678643879fa04aa5fd7e9616451ba
FanGuo <fguo10@hawk.iit.edu>
2023-05-15 19:33:40 -0500

    delete external project: time demo

--- time_demo/src/main/java/com/example/time_demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.time_demo.repository;
-
-import com.example.time_demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
